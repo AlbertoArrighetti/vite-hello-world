137fc55c3c844ef16661f1ea7b0f24e7b2e55b61
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-28 18:34:43 +0100

    jumbotron images added

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -9,26 +9,34 @@ export default {
 <template>
     <!-- questo verra visualizzato in pagina -->
     <div id="jumbotron">
-        <h1 class="jumbotron_title">
-            Titolo Jumbotron
-        </h1>
+        <div class="text_container">
+            <h1 class="jumbotron_title b_color_op">
+                Titolo Jumbotron
+            </h1>
+            <p class="jumbotron_p b_color_op">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quis. Nobis assumenda inventore corrupti eos aut numquam nemo aperiam atque debitis maiores fuga, ad molestias, architecto, quaerat rerum voluptas ullam.
+            </p>
+        </div>
+        <div class="img_container">
+            <img src="../assets/vue.svg" alt="vue image">
+            <img src="../../public/vite.svg" alt="vite image">
+        </div>
     
-        <p class="jumbotron_p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quis. Nobis assumenda inventore corrupti eos aut numquam nemo aperiam atque debitis maiores fuga, ad molestias, architecto, quaerat rerum voluptas ullam.
-        </p>
     </div>
 </template>
 
 <style>
-
 #jumbotron{
     max-width: 1400px;
     margin: 0 auto;
     padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
 
     background-color: #C5EBAA;
 }
-.jumbotron_title, .jumbotron_p{
+
+.b_color_op{
     color: black;
     opacity: 0.8;
 }
@@ -37,5 +45,18 @@ export default {
 }
 .jumbotron_p {
     padding: 0 20px;
+}
+.text_container{
+    max-width: 1000px;
+}
+.img_container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+    gap: 20px;
+}
+.img_container img{
+    height: 100px;
 }
 </style>
