28bad21e735cf7d3491d415bfad5e9cc4914ce59
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-28 15:14:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+// importa il componente con:
+import AppTitle from './components/AppTitle.vue';
+
+export default{
+  // inserisco il mio componente con components: 
+  components: {
+    AppTitle,
+  },
+}
+
+
+</script>
+
+<template>
+    
+    <AppTitle></AppTitle>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+// prelevo il mio componente
+export default {
+    name: 'AppTitle'
+}
+
+</script>
+
+<template>
+    <!-- questo verra visualizzato in pagina -->
+    <h1>
+        La mia prima app con Vite!
+    </h1>
+</template>
+
+<style>
+/* qui posso aggiungere il suo stile */
+h1{
+    color: #B7C9F2;
+    text-align: center;
+}
+</style>
